c717d3f06fc29d5f132f854ce588ede7a7af1625
Krechuk <58669215+krechuk@users.noreply.github.com>
2024-09-25 19:04:12 +0300

    Add files via upload

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,26 @@
+
+
+
+const scriptsInEvents = {
+
+	async Mainjs_Event1(runtime, localVars)
+	{
+		let initialHeight = window.innerHeight;
+		
+		window.addEventListener('resize', function() {
+		    let newHeight = window.innerHeight;
+		    if (newHeight < initialHeight) {
+		        // Клавиатура открыта
+		        document.getElementById('gameContainer').style.height = initialHeight + 'px';
+		    } else {
+		        // Клавиатура закрыта
+		        document.getElementById('gameContainer').style.height = '100%';
+		    }
+		});
+		
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
